82efa9551b7c4e46f9193b1b2ac1d13f57b54822
Abdul Shakoor Abdul Azeem Mousa <shoukreytom01@gmail.com>
2021-09-10 21:16:44 +0200

    code: fetch screenshots of portfolio

--- src/pages/PortfolioDetails.js
+++ src/pages/PortfolioDetails.js
@@ -8,6 +8,7 @@ class PortfolioDetails extends React.Component {
       error: null,
       isLoaded: false,
       item: undefined,
+      screenshots: [],
     };
   }
 
@@ -15,28 +16,26 @@ class PortfolioDetails extends React.Component {
     let id = this.props.match.params.id;
     const url = `https://shoukrey.herokuapp.com/api/portfolio/${id}/`;
     fetch(url)
-      .then((res) => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            item: result,
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error,
-          });
-        }
-      );
+    .then((res) => res.json())
+    .then((data) => {
+      this.setState({isLoaded: true, item: data});
+      },
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error,
+        });
+      }
+    );
+    fetch(`${url}screenshots/`)
+    .then((res) => res.json())
+    .then((data) => {
+      this.setState({screenshots: data});
+    });
   }
 
   render() {
-    const { error, isLoaded, item } = this.state;
+    const { error, isLoaded, item, screenshots } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
@@ -64,9 +63,19 @@ class PortfolioDetails extends React.Component {
           <Header />
           <div className="container px-4 py-5 my-5 w-100">
             <img className="img-fluid" src={item.image} alt={item.title} />
-            <h1 className="display-5 fw-bold">{item.title}</h1>
+            <h1 className="display-5 fw-bold my-4">{item.title}</h1>
             <p className="lead ps-4">{item.description}</p>
             {urls}
+            <div className="container py-5 w-100">
+              <h1>Screenshots</h1>
+              {screenshots.map(item => (
+                <div className="row w-100">
+                  <div className="col m-4">
+                    <img className="img-fluid" key={item.id} src={item.image} alt={`screenshot ${item.id}`}/>
+                  </div>
+                </div>
+            ))}
+            </div>
           </div>
         </div>
       );
